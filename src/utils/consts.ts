90b866fa748d3bdfd3159e080f15eccd01157382
Arslon Saidov <arslonbeksaidov@users.noreply.github.com>
2026-03-19 17:30:54 +0500

    Update API_BASE URL to new endpoint

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,4 +1,4 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || "https://bot.tavis.uz/api";
+export const API_BASE = import.meta.env.VITE_API_BASE || "https://smart-bot-api.vincere.uz/api";
 
 export const LS_ACCESS = "didox_access";
 export const LS_REFRESH = "didox_refresh";
