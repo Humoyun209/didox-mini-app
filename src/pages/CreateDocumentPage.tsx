b810bdef9db6e23652755696857c6d71f7289c9e
humoyun209 <h.ahmedov209@gmail.com>
2026-03-26 13:31:51 +0500

    5

--- src/pages/CreateDocumentPage.tsx
+++ src/pages/CreateDocumentPage.tsx
@@ -390,7 +390,7 @@ export default function CreateDocumentPage({
                           : "border-zinc-800 hover:border-zinc-600"
                       }`}
                     >
-                      <span className="text-white">File URL: {doc.file}</span>
+                      {/* <span className="text-white">File URL: {doc.file}</span> */}
                       {/* Radio indicator */}
                       <div
                         className={`mt-0.5 w-4 h-4 rounded-full border-2 flex items-center justify-center flex-shrink-0 transition-all ${
